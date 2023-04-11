153ad44740a1bb61bf21bad7e9e802239c2868c9
阿豪 <2670321326@qq.com>
2023-04-11 20:45:24 +0800

    Tabbar功能

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import Tabbar from './commponents/tabbar/Tabbar.vue';
 </script>
 
 <template>
- <h1>vueapp</h1>
- <Tabbar></Tabbar>
+    <h1>vueapp</h1>
+    <router-view></router-view>
 </template>
 
 <style scoped></style>
--- src/commponents/tabbar/Tabbar.vue
+++ src/commponents/tabbar/Tabbar.vue
@@ -1,13 +1,16 @@
 <template>
-<router-view></router-view>
-<van-tabbar v-model="active">
-  <van-tabbar-item icon="home-o" to="/home">首页</van-tabbar-item>
-  <van-tabbar-item icon="search" to="/message">消息</van-tabbar-item>
-  <van-tabbar-item icon="friends-o" to="collect">收藏</van-tabbar-item>
-  <van-tabbar-item icon="setting-o" to="order">订单</van-tabbar-item>
-</van-tabbar>
+  <router-view></router-view>
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="home-o" to="/home">首页</van-tabbar-item>
+    <van-tabbar-item icon="search" to="/message">消息</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" to="collect">收藏</van-tabbar-item>
+    <van-tabbar-item icon="setting-o" to="order">订单</van-tabbar-item>
+  </van-tabbar>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const active = ref(0)
+</script>
 
 <style scoped lang="less"></style>
